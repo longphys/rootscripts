f30b883e45221fa603cdbd18b5025e4f1bf3c567
Long <longphys@gmail.com>
2025-02-17 17:47:38 +0300

    commit

--- neutron/declib/read2.C
+++ neutron/declib/read2.C
@@ -13,8 +13,8 @@
 #include "TGraph.h"
 
 //! pixels, 1 pixel - diameter = 3,5cm
-double distance_from_center[] = {0, 3.5, 7.0, 10.5, 14.0, 17.5, 21.0};
-
+// double distance_from_center[] = {0, 3.5, 7.0, 10.5, 14.0, 17.5, 21.0};
+double size_of_pixel = 2.; //cm
 //! Proton cross-talk
 // 8x8: 2,6%; 8x9: 5,2%; 8x10: 12,8%
 double proton_cross_talk[] = {2.6, 4.5, 5.0, 2.8, 1.9, 1.3, 1.0};
@@ -60,19 +60,19 @@ void read2(){
   gr1->GetYaxis()->SetTitleSize(0.04);
   gr1->GetYaxis()->CenterTitle(true);
 
-  for (int i = 0; i < sizeof(distance_from_center)/sizeof(distance_from_center[0]); i++){
+  for (int i = 0; i < sizeof(proton_cross_talk)/sizeof(proton_cross_talk[0]); i++){
     std::cout << i << "\n";
-    gr1->AddPoint(distance_from_center[i], proton_cross_talk[i]);
-    std::cout << "distance = " << distance_from_center[i] << "; cross-talk = " << proton_cross_talk[i] << "\n";
-    gr2->AddPoint(distance_from_center[i], neutron_mis_hits_neighbor[i]);
-    std::cout << "distance = " << distance_from_center[i] << "; mis_hits_neighbor = " << neutron_mis_hits_neighbor[i] << "\n";
-    gr3->AddPoint(distance_from_center[i], neutron_mis_hits_center[i]);
-    std::cout << "distance = " << distance_from_center[i] << "; mis_hits_center = " << neutron_mis_hits_center[i] << "\n";
+    gr1->AddPoint(2*i, proton_cross_talk[i]);
+    std::cout << "distance = " << 2*i << "; cross-talk = " << proton_cross_talk[i] << "\n";
+    gr2->AddPoint(2*i, neutron_mis_hits_neighbor[i]);
+    std::cout << "distance = " << 2*i << "; mis_hits_neighbor = " << neutron_mis_hits_neighbor[i] << "\n";
+    gr3->AddPoint(2*i, neutron_mis_hits_center[i]);
+    std::cout << "distance = " << 2*i << "; mis_hits_center = " << neutron_mis_hits_center[i] << "\n";
   }
 
   c_1->cd();
   gr1->Draw("ALP");
-  gr1->GetXaxis()->SetRangeUser(-1.0, 22.0);
+  gr1->GetXaxis()->SetRangeUser(-1.0, 13.0);
   gr1->GetYaxis()->SetRangeUser(0., 100.);
   gr2->Draw("LP SAME");
   gr3->Draw("LP SAME");
@@ -89,6 +89,7 @@ void read2(){
 
   TCanvas* c_2 = new TCanvas("c_2", "c_2", 1000, 700);
   c_2->cd();
+
   gr1->Draw("ALP");
   // gr1->GetXaxis()->SetRangeUser(-1.0, 10.0);
   // gr1->GetYaxis()->SetRangeUser(0., 200.);
@@ -99,9 +100,9 @@ void read2(){
   legend1->SetBorderSize(0);
   legend1->SetLineWidth(2);
   legend1->SetTextSize(0.04);
-  legend1->AddEntry(gr1, "Proton cross-talk", "l");
-  legend1->AddEntry(gr2, "#splitline{Neutron mis-hits to}{the neighboring module}", "l");
+  // legend1->AddEntry(gr1, "Proton cross-talk", "l");
+  // legend1->AddEntry(gr2, "#splitline{Neutron mis-hits to}{the neighboring module}", "l");
   // legend->AddEntry(gr3, "Neutron mis-hits \n to the center module \n", "l");
 
-  legend1->Draw();
+  // legend1->Draw();
 }
